b2955465725f89567669025ae140e431a98db373
Veronica De Leon <v.dleon@gmail.com>
2020-11-24 11:28:37 +0100

    react-icons / react-toastify

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import { IconContext } from "react-icons"
+import { FaReact } from "react-icons/fa"
+import { MdAlarm } from "react-icons/md"
+
+import { toast } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
+
+const CustomToast = ({ closeToast }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Something went wrong!
+      <button onClick={closeToast}>Close</button>
     </div>
+  )
+}
+
+toast.configure()
+
+function App() {
+
+  const notify = () => {
+    toast("Basic notification", {position: toast.POSITION.TOP_LEFT})
+    toast.success("Success notification", {position: toast.POSITION.TOP_CENTER, autoClose: 8000})
+    toast.info("Info notification", {position: toast.POSITION.TOP_RIGHT, autoClose: false})
+    toast.warn(<CustomToast/>, {position: toast.POSITION.BOTTOM_LEFT})
+    toast.error("Error notification", {position: toast.POSITION.BOTTOM_CENTER})
+    toast("Basic notification", {position: toast.POSITION.BOTTOM_RIGHT})
+  }
+
+  return (
+    <IconContext.Provider value={{ color:"blue", size:"5rem"}} >
+    <div className="App">
+      <FaReact />
+      <MdAlarm />
+
+      <button onClick={notify}>Notify</button>
+    </div>
+    </IconContext.Provider>
   );
 }
 
